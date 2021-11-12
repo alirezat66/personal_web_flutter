097e34931ae0de8a1885f8a2d36627174244b5c0
alireza taghizadeh <alirezataghizadeh66@gmail.com>
2021-11-12 11:29:04 +0300

    make contact responsive

--- lib/screen/contact_screen.dart
+++ lib/screen/contact_screen.dart
@@ -17,14 +17,169 @@ class ContactScreen extends StatelessWidget {
         ? MediaQuery.of(context).size.width - 320
         : MediaQuery.of(context).size.width;
 
+    return ResponsiveCheker.isDeviceMobileWithContext(context)
+        ? ContactMobileWidget(fullWidth: fullWidth)
+        : ContactDesktopAndTabletWidget(
+            fullWidth: fullWidth,
+          );
+  }
+}
+
+class ContactMobileWidget extends StatelessWidget {
+  final double fullWidth;
+  const ContactMobileWidget({Key? key, required this.fullWidth})
+      : super(key: key);
+  getMap() {
+    String htmlId = "7";
+
+    // ignore: undefined_prefixed_name
+    ui.platformViewRegistry.registerViewFactory(htmlId, (int viewId) {
+      final myLatlng = gMap.LatLng(38.5134829, 27.036787);
+
+      final mapOptions = gMap.MapOptions()
+        ..zoom = 14
+        ..center = gMap.LatLng(38.5134829, 27.036787);
+
+      final elem = DivElement()
+        ..id = htmlId
+        ..style.width = "100%"
+        ..style.height = "100%"
+        ..style.border = 'none';
+
+      final map = gMap.GMap(elem, mapOptions);
+
+      gMap.Marker(gMap.MarkerOptions()
+        ..position = myLatlng
+        ..map = map
+        ..title = 'This is my place!');
+
+      return elem;
+    });
+
+    return HtmlElementView(viewType: htmlId);
+  }
+
+  @override
+  Widget build(BuildContext context) {
     return Container(
         width: fullWidth,
         color: Color(0xff191c26),
         child: Padding(
             padding: EdgeInsets.symmetric(
-              horizontal: (ResponsiveCheker.isDeviceDesktopWithContext(context)
-                  ? ConstantsValue.kLargeLeftRightPadding
-                  : ConstantsValue.kSmallLeftRightPadding)),
+                horizontal:
+                    (ResponsiveCheker.isDeviceDesktopWithContext(context)
+                        ? ConstantsValue.kLargeLeftRightPadding
+                        : ConstantsValue.kSmallLeftRightPadding)),
+            child:
+                Column(crossAxisAlignment: CrossAxisAlignment.start, children: [
+              SizedBox(
+                height: ConstantsValue.kMainTopAndBottomPadding,
+              ),
+              Text(
+                'Contact',
+                style: Theme.of(context).textTheme.headline2,
+              ),
+              SizedBox(
+                height: ConstantsValue.kTitleAndSubtitlePadding,
+              ),
+              Text('You can contact me whit below contact info.',
+                  style: Theme.of(context).textTheme.subtitle1),
+              SizedBox(
+                height: ConstantsValue.kSmallPaddingBetweenTwoLayout,
+              ),
+              Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    mainAxisAlignment: MainAxisAlignment.start,
+                    mainAxisSize: MainAxisSize.max,
+                    children: [
+                      ContactWidget(
+                        icon: IconlyBroken.call,
+                        title: 'Phone',
+                        description: '+905366526420',
+                        width: fullWidth * 0.8,
+                      ),
+                      SizedBox(
+                          height: ConstantsValue.kSubtitleAndCaptionPadding),
+                      ContactWidget(
+                        icon: IconlyBroken.chat,
+                        title: 'Gmail',
+                        description: 'alirezataghizadeh66@gmail.com',
+                        width: fullWidth * 0.8,
+                      ),
+                      SizedBox(
+                          height: ConstantsValue.kSubtitleAndCaptionPadding),
+                      ContactWidget(
+                        icon: IconlyBroken.location,
+                        title: 'Address:',
+                        description: 'Turkey, Izmir, Balatcik',
+                        width: fullWidth * 0.8,
+                      ),
+                    ],
+                  ),
+                  SizedBox(
+                    height: ConstantsValue.kSmallLeftRightPadding,
+                  ),
+                  AspectRatio(
+                    aspectRatio: 1,
+                    child: getMap(),
+                  ),
+                ],
+              ),
+              SizedBox(
+                height: 160,
+              )
+            ])));
+  }
+}
+
+class ContactDesktopAndTabletWidget extends StatelessWidget {
+  final double fullWidth;
+  const ContactDesktopAndTabletWidget({Key? key, required this.fullWidth})
+      : super(key: key);
+  getMap() {
+    String htmlId = "7";
+
+    // ignore: undefined_prefixed_name
+    ui.platformViewRegistry.registerViewFactory(htmlId, (int viewId) {
+      final myLatlng = gMap.LatLng(38.5134829, 27.036787);
+
+      final mapOptions = gMap.MapOptions()
+        ..zoom = 14
+        ..center = gMap.LatLng(38.5134829, 27.036787);
+
+      final elem = DivElement()
+        ..id = htmlId
+        ..style.width = "100%"
+        ..style.height = "100%"
+        ..style.border = 'none';
+
+      final map = gMap.GMap(elem, mapOptions);
+
+      gMap.Marker(gMap.MarkerOptions()
+        ..position = myLatlng
+        ..map = map
+        ..title = 'This is my place!');
+
+      return elem;
+    });
+
+    return HtmlElementView(viewType: htmlId);
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+        width: fullWidth,
+        color: Color(0xff191c26),
+        child: Padding(
+            padding: EdgeInsets.symmetric(
+                horizontal:
+                    (ResponsiveCheker.isDeviceDesktopWithContext(context)
+                        ? ConstantsValue.kLargeLeftRightPadding
+                        : ConstantsValue.kSmallLeftRightPadding)),
             child:
                 Column(crossAxisAlignment: CrossAxisAlignment.start, children: [
               SizedBox(
@@ -91,35 +246,5 @@ class ContactScreen extends StatelessWidget {
                 height: 160,
               )
             ])));
-  }
-
-  getMap() {
-    String htmlId = "7";
-
-    // ignore: undefined_prefixed_name
-    ui.platformViewRegistry.registerViewFactory(htmlId, (int viewId) {
-      final myLatlng = gMap.LatLng(38.5134829, 27.036787);
-
-      final mapOptions = gMap.MapOptions()
-        ..zoom = 14
-        ..center = gMap.LatLng(38.5134829, 27.036787);
-
-      final elem = DivElement()
-        ..id = htmlId
-        ..style.width = "100%"
-        ..style.height = "100%"
-        ..style.border = 'none';
-
-      final map = gMap.GMap(elem, mapOptions);
-
-      gMap.Marker(gMap.MarkerOptions()
-        ..position = myLatlng
-        ..map = map
-        ..title = 'This is my place!');
-
-      return elem;
-    });
-
-    return HtmlElementView(viewType: htmlId);
   }
 }
