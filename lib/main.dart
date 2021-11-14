485ef152b14b7adf489306c344f628b18e80c78c
alireza taghizadeh <alirezataghizadeh66@gmail.com>
2021-11-14 14:55:12 +0300

    finish the settings

--- lib/main.dart
+++ lib/main.dart
@@ -10,7 +10,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Flutter Developer',
       debugShowCheckedModeBanner: false,
       theme: ThemeData(
         primaryColor: Color(0xff191c26),
